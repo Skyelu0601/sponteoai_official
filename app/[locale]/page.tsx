75bbf827ccafa176bc4a7577c195ca7c24d6526e
Skyelu0601 <luwen_123456@126.com>
2025-11-07 11:34:46 +0800

    Fix next-intl static rendering issues

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,16 @@
+import { setRequestLocale } from 'next-intl/server';
 import Hero from '@/components/Hero';
 import AboutSection from '@/components/AboutSection';
 import ServicesSection from '@/components/ServicesSection';
 import ContactSection from '@/components/ContactSection';
 
-export default function Home() {
+export default function Home({
+  params: { locale }
+}: {
+  params: { locale: string };
+}) {
+  setRequestLocale(locale);
+
   return (
     <div>
       <Hero />
